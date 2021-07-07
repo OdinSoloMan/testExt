bf3767d16a31d59688f52c11e08d88b807482f03
OdinSolo <yuraburov18@gmail.com>
2021-07-07 18:13:53 +0300

    Made a read from the controller to the frontend (Сделал считывание с контроллера  на frontend)

--- FrontEnd/Controllers/HomeController.cs
+++ FrontEnd/Controllers/HomeController.cs
@@ -30,14 +30,29 @@ namespace FrontEnd.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
-            return View();
+            var products = new List<FrontEnd.Models.Products>
+            {
+                new FrontEnd.Models.Products {Id = 1 , Name = "Test Name1", Description = "Test Description1"},
+                new FrontEnd.Models.Products {Id = 2 , Name = "Test Name2", Description = "Test Description2"},
+                new FrontEnd.Models.Products {Id = 3 , Name = "Test Name3", Description = "Test Description3"},
+                new FrontEnd.Models.Products {Id = 4 , Name = "Test Name4", Description = "Test Description4"},
+                new FrontEnd.Models.Products {Id = 4 , Name = "Test NameYes", Description = "Test DescriptionYes"}
+            };
+
+
+            return View(products.ToList());
         }
 
         public ActionResult Orders()
         {
             ViewBag.Message = "Orders.";
-
-            return View();
+            var orders = new List<FrontEnd.Models.Orders>
+            {
+                new FrontEnd.Models.Orders { Id = 1, Name="test", Count = 4 },
+                new FrontEnd.Models.Orders { Id = 2, Name="test1", Count = 2 },
+                new FrontEnd.Models.Orders { Id = 3, Name="test2", Count = 5 }
+            };
+            return View(orders.ToList());
         }
 
         public ActionResult CountBy(int count)
---
+++ FrontEnd/Models/Products.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FrontEnd.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
