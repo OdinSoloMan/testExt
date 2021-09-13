457bb30cdfcccfa0bb18b4ef43e3d8018fd6bcca
OdinSolo <yuraburov18@gmail.com>
2021-09-13 19:02:34 +0300

    Creating a data model and implementing their repository

---
+++ MyProjects/Backend-MyTask/DataAccess/User.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend_MyTask.DataAccess
+{
+    [MetadataType(typeof(UserMetadata))]
+    [Index("Username", IsUnique = true, Name = "Username_Index")]
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public ICollection<Task> Tasks { get; set; }
+    
+        public User()
+        {
+            Id = new Guid();
+            Username = "";
+            Password = "";
+        }
+    }
+
+    internal sealed class UserMetadata
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(50, ErrorMessage = "Username length it 3 before 50", MinimumLength = 3)]
+        public string Username;
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(50, ErrorMessage = "Password length it 5 before 50", MinimumLength = 5)]
+        public string Password;
+    }
+}
---
+++ MyProjects/Backend-MyTask/Service/Entity/IBoardRepository.cs
@@ -0,0 +1,17 @@
+﻿using Backend_MyTask.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_MyTask.Service.Entity
+{
+    public interface IBoardRepository
+    {
+        Task<IEnumerable<Board>> ReadAll();
+        Task Create(Board board);
+        Task<Board> Read(Guid id);
+        Task Update(Board board);
+        Task Delete(Guid id);
+    }
+}
